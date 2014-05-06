c2c0dcee6f5c2013d822936f10f17d5ae0398765
Hack Reactor Students <talegram@gmail.com>
2014-05-05 21:16:18 -0700

    implements POST and GET functionality

--- server/request-handler.js
+++ server/request-handler.js
@@ -5,7 +5,7 @@
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
 
-exports.handleRequest = function(request, response) {
+exports.handler = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
   request - such as what URL the browser is requesting. */
 
@@ -14,22 +14,45 @@ exports.handleRequest = function(request, response) {
 
   console.log("Serving request type " + request.method + " for url " + request.url);
 
-  var statusCode = 200;
-
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
+  var statusCode = 200;
   var headers = defaultCorsHeaders;
-
   headers['Content-Type'] = "text/plain";
 
+  var data = '';
+  // request.setEncoding('utf8');
+  request.on('data', function(bit){
+
+      data += bit;
+  });
+
+  request.on('end', function() {
+    var body = 'Hello, World';
+    // if (request.url !== '/classes/messages') {
+      // statusCode = 404;
+    if (request.method === 'POST') {
+      storage.results.push(JSON.parse(data));
+      console.log(storage.results);
+      body = JSON.stringify(storage);
+      statusCode = 201;
+    } else if (request.method === 'GET') {
+      statusCode = 200;
+      body = JSON.stringify(storage);
+    }
+    response.writeHead(statusCode, headers);
+    response.end(body);
+  });
+
+
   /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
+  // response.writeHead(statusCode, headers);
 
   /* Make sure to always call response.end() - Node will not send
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, World!");
+  // response.end("Hello, World!");
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -43,3 +66,21 @@ var defaultCorsHeaders = {
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
 };
+
+var storage = {results: []};
+
+var chatMaker = function(createdAt, username, text, roomname) {
+  return { createdAt: createdAt,
+    username: username,
+    text: text,
+    roomname: roomname
+  };
+};
+
+// if (request.url === '/classes/messages') {
+// createdAt: "2013-10-07T16:22:03.280Z"
+// objectId: "teDOY3Rnpe"
+// roomname: "lobby"
+// text: "hello"
+// updatedAt: "2013-10-07T16:22:03.280Z"
+// username: "gary"
